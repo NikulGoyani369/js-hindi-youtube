bceeb0b7b780160c6437f44e2322e2f8d5cc5253
Nikul Goyani <goyaniniks@gmail.com>
2023-09-12 06:01:33 +0000

    deep dive into data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3); // this is not good practise please write a single code 
+        // becasue of code readability should be high
+
+console.log("Nirmal Goyani");
+
+
+let name = "Nirmal"
+let age = 29
+let isLoggedIn = true
+
+
+// number => ka range 2 to power of 53 che 
+// bigint jyare number moto hoi tyare use thai che 
+// string => ""
+//  boolean => true / false
+// null => standalone value // null ne aape let state = null api 6kiye jem null etle empty value 
+// undefined => define kri hoi pan value appi nhi hoi 
