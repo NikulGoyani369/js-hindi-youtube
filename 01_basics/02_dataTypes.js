d198179c68c8b5bfedde9d86a906a23ad2192240
Nikul Goyani <goyaniniks@gmail.com>
2024-02-10 12:15:48 +0100

    Update 02_dataTypes.js
    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,21 +1,37 @@
 "use strict"; // treat all JS code as newer version
 
-
-// alert( 3 + 3) // we are using nodejs, not browser che
+// alert( 3 + 3) // we are using nodejs, not browser... jyare aa alert() browser ma use krvama ave tyare popup dekhai che 
 
 console.log(3 +  3); // this is not good practise please write a single code  // becasue of code readability should be high
 
+// JS Documentation mate mozila mdn use krvanu https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+// Orignal JS Documentation for je engine banave che like NodeJS or V8 mate https://tc39.es/ecma262/
+
 console.log("Nirmal Goyani");
 
+let name = "Nirmal" // string dataType
+let age = 29 // number DataType
+let isLoggedIn = true // boolean dataType
 
-let name = "Nirmal"
-let age = 29
-let isLoggedIn = true
-
-
+/*niche badhi primitive data type che*/
 // number => ka range 2 to power of 53 che 
 // bigint jyare number moto hoi tyare use thai che 
 // string => ""
-//  boolean => true / false
+// boolean => true / false
 // null => standalone value // null ne aape let state = null api 6kiye jem null etle empty value 
 // undefined => define kri hoi pan value appi nhi hoi 
+// symbol => jyare unique ni vat krvama ave tyare symnol dataType use karvama ave che
+
+
+// nul type ek object che  // it's interview question
+
+// Non-Primitive Data Type	Explanation
+// Object	=> It is an instance that allows us to access members in JavaScript.
+// Array	=> It represents a collection of similar elements in JavaScript.
+// RegExp	=> It represents a regular expression in JavaScript.
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
