099059cfeb027cfbc5027b3f426e039739aed332
Nikul Goyani <goyaniniks@gmail.com>
2024-02-10 10:18:55 +0000

    notes updates

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,21 +2,23 @@
 /**
  * below code is variable declared Info.....
  */
+//const = can't be re-assigned a value and can't be declared again  (block scope)
+const accountId = 144553            // constant means asign value never change
 
-const accountId = 144553            // constant means asign value never change 
+//let = can be re-assigned a value but can't be declared again (block scope)
 let accountEmail = "nirmal@google.com" // let means will change value or 
-var accountPassword = "Test1234"       // 
-accountCity = "Jaipur"                //
+
+//var = it can be re-assigned a value and it can also be declared again  (Global scope)
+var accountPassword = "Test1234"       
+accountCity = "Jaipur"                
 let accountState;
 
 // accountId = 2
 console.log(accountId);
 
-
 accountEmail = "ng@gmail.com"
 accountPassword = "123Test"
-accountCity = "Surat"
-
+accountCity = "Surat" 
 
 /* 
 Prefer not to use var variable
