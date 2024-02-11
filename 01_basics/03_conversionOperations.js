964353e84d00b31b9cb75b3b8db503897864c61c
Nikul Goyani <goyaniniks@gmail.com>
2024-02-11 13:10:15 +0000

    add docu link in conversionOperation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -55,6 +55,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
 // 33 => "33"
 
+// ******************** Operations ******************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// basic operation 
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+
+let str1 = "hello"
+let str2 = " Nirmal"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+console.log(+true);  // tricky conversion with nosense output 
+console.log(+"");  // tricky conversion with nosense
+// +true => 1
+// +"" => 0
+
+
+// link to study  https://tc39.es/ecma262/#sec-abstract-operations
+
+// prefix unary operator and postfix unary operator
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
